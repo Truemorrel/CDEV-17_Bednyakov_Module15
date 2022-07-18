14eea1cc36763f9bebb38487255c2a187b76c4e5
Sergey Bednyakov <sbednyakov@gmail.com>
2022-07-18 04:37:07 +0300

    master: add task 15.1.6

---
+++ Task_15_1_6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_15_1_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] spacers = new char[] { ' ', ',', '.', ';', ':', '?', '!', '\''};
+            var inputString = Console.ReadLine();
+            if (string.IsNullOrEmpty(inputString))
+            {
+                Console.WriteLine("Введите непустую строку");
+            }
+            IEnumerable<char> result = inputString.ToCharArray().Except(spacers);
+            result = result.Union(result);
+            Console.WriteLine(result.ToArray()); ;
+        }
+    }
+}
